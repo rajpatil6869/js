a0178e055c8e171d03cfcf4fb7e4e87cb1dcc506
rajpatil6869 <146859067+rajpatil6869@users.noreply.github.com>
2025-01-29 09:45:44 +0000

    datatypes complete

--- 01_Basics/02_Datatypes.js
+++ 01_Basics/02_Datatypes.js
@@ -1,1 +1,35 @@
 "use strict"; //treat all js code as newer version
+
+// alert("raj");  we are using node js , not browser
+
+console.log(3
+    +
+    3) //code readability should be high
+
+console.log("Raj");
+
+let name = "Raj";      // string
+let age = 22;          // number
+let isloggedIn = true; // boolean
+let state ;            // undefined
+
+console.table([name,age,isloggedIn]);
+
+// primitive datatypes
+        
+// number => 2 to power of 53
+// bigint
+// String => '' or ""
+// boolean => true of false
+// null => standalone value
+// undefined 
+// symbol => uniqueness
+
+
+// object
+
+console.log(typeof state);
+console.log(typeof null);       // datatype of null is object
+console.log(typeof undefined);  // datatype of undefined is undefined
+console.log(typeof "raj");
+
